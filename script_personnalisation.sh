719d53643de505e8a8135e7b452a0a4182e32308
Siete7777 <oumar.lazare@gmail.com>
2024-03-26 07:41:36 +0100

    nouvelle version bash

--- script_personnalisation.sh
+++ script_personnalisation.sh
@@ -1,13 +1,23 @@
-#/bin/bash
+#!/bin/bash
 
-docker exec -it projet_devops apt install unzip
-docker exec -it owncloud cd /var/www/owncloud/apps
-docker exec -it owncloud wget https://github.com/owncloud/theme-example/archive/master.zip
-docker exec -it owncloud unzip master.zip
-docker exec -it owncloud rm master.zip
-docker exec -it owncloud mv theme-example-master mynewtheme
-docker exec -it owncloud sed -i "s#<id>theme-example<#<id>mynewtheme<#" "mynewtheme/appinfo/info.xml"
-docker exec -it owncloud sudo chown -R www-data: mynewtheme
-docker exec -it owncloud sudo -u www-data ./occ app:enable mynewtheme
+# Installer unzip dans le conteneur projet_devops
+docker exec -it projet_devops apt install -y unzip
 
-docker exec -it owncloud sudo cd /var/www/owncloud/core/img | mv background.jpg old_background.jpg | wget -O background.jpg https://www.eliott-markus.com/wp-content/uploads/2023/08/Groupe-1931@2x-min.png.webp
+# Télécharger et installer le thème personnalisé
+docker exec -it owncloud bash -c ' \
+    cd /var/www/owncloud/apps && \
+    wget https://github.com/owncloud/theme-example/archive/master.zip && \
+    unzip master.zip && \
+    rm master.zip && \
+    mv theme-example-master mynewtheme && \
+    sed -i "s#<id>theme-example<#<id>mynewtheme<#" "mynewtheme/appinfo/info.xml" && \
+    chown -R www-data:www-data mynewtheme && \
+    sudo -u www-data php occ app:enable mynewtheme \
+'
+
+# Mettre à jour l'image de fond
+docker exec -it owncloud bash -c ' \
+    cd /var/www/owncloud/core/img && \
+    mv background.jpg old_background.jpg && \
+    wget -O background.jpg https://www.eliott-markus.com/wp-content/uploads/2023/08/Groupe-1931@2x-min.png.webp \
+'
